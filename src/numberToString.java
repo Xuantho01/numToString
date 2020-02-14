878eb8205ca2e3c702696da0e99ad27e0d1608e5
Xuantho01 <hoxuantho95@gmail.com>
2020-02-14 11:14:56 +0700

    update file

---
+++ src/numberToString.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class numberToString {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num,digit1,digit2,digit3,tmp;
+        String str1[] = {"","","twenty","thirty","Forty","fifty","sixty","seventy","eighty","ninety"};
+        String str2[] = {"","one","two","three","four","five","six","seven","eight","nine","ten","eleven",
+        "twelve","thirteen","fourteen","fifteen","sixteen","nineteen"};
+        System.out.println("Enter any number between 1 to 99: ");
+
+        num = input.nextInt();
+
+        if((num <= 1) || (num >= 1000)){
+            System.out.println("Wrong input!");
+        }else if((num >= 1) && (num <= 19)){
+            System.out.println(str2[num]);
+        }
+        else if((num >= 1) && (num <= 99)){
+            digit1 = num/10;
+            digit2 = num%10;
+            System.out.println(str1[digit1]+ " " + str2[digit2]);
+        }else if((num >= 1) && (num <= 999)){
+            digit1 = num/100;
+            tmp = num%100;
+            digit2 = tmp/10;
+            digit3 = tmp%10;
+            System.out.println(str2[digit1] + " hundred "+ str1[digit2] + " "+ str2[digit3]);
+        }
+    }
+}
